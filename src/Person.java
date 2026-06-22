d83b979b2ab381469164cd88424c1e6218540a6a
Logan Marsh <loganm92304@gmail.com>
2026-06-22 12:13:43 -0230

    Implemented Person superclass

---
+++ src/Person.java
@@ -0,0 +1,54 @@
+public class Person {
+
+    protected int id;
+    protected String name;
+    protected int age;
+    protected String phoneNumber;
+
+    public Person(int id, String name, int age, String phoneNumber) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+        this.phoneNumber = phoneNumber;
+    }
+
+    // Getters
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    // Setters (needed later for edit functionality)
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    @Override
+    public String toString() {
+        return "ID: " + id +
+               ", Name: " + name +
+               ", Age: " + age +
+               ", Phone: " + phoneNumber;
+    }
+}
